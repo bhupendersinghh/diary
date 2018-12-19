117dad3f201b42396fb905bbb0549770d0885704
bhupendersinghh <38017615+bhupendersinghh@users.noreply.github.com>
2018-12-19 18:12:50 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Secret Diary</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<style type="text/css">
+		.container {
+      
+            text-align: center;
+            width: 400px;
+      }
+          
+      #homePageContainer {
+              
+            margin-top: 150px;
+          
+      }
+          
+          #containerLoggedInPage {
+              
+              margin-top: 60px;
+              
+          }
+          
+          
+      html { 
+          
+          background: url(https://images.unsplash.com/photo-1540205081-d01d494b6f37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          
+          }
+          
+          body {
+              
+              background: none;
+              color: #FFF;
+              
+          }
+          
+          #logInForm {
+              
+              display:none;
+              
+          }
+          
+          .toggleForms {
+              
+              font-weight: bold;
+              
+          }
+          
+          #diary {
+              
+              width: 100%;
+              height: 100%;
+              
+          }
+          
+      </style>
+  </head>
+  <body>
